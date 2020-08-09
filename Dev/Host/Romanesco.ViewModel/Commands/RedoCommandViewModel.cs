52304dd5089eb13ef8373f3f5a7c0d46ddb90c1c
NumAniCloud <num.ani.cloud@gmail.com>
2020-08-09 18:33:54 +0900

    クラスの抽出:RedoCommandViewModel

---
+++ Dev/Host/Romanesco.ViewModel/Commands/RedoCommandViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Romanesco.Common.Model.Helpers;
+using Romanesco.Model.Interfaces;
+
+namespace Romanesco.ViewModel.Commands
+{
+	internal class RedoCommandViewModel : CommandViewModel
+	{
+		private readonly ICommandAvailabilityPublisher model;
+
+		public RedoCommandViewModel(ICommandAvailabilityPublisher model, BooleanUsingScopeSource commandExecution)
+			: base(model.CanRedo, commandExecution)
+		{
+			this.model = model;
+		}
+
+		public override void Execute(object? parameter) => model.Redo();
+	}
+}
